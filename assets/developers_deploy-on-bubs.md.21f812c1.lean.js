32e8bb6b5b72d8d9d37a1cc923f991c4ce9d097c
jueduizone <jueduizone@gmail.com>
2023-07-20 12:01:59 +0000

    Deploy by source 🚀

---
+++ assets/developers_deploy-on-bubs.md.21f812c1.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t}from"./chunks/framework.1cfd022e.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"developers/deploy-on-bubs.md","filePath":"developers/deploy-on-bubs.md"}'),s={name:"developers/deploy-on-bubs.md"};function r(a,n,p,d,c,l){return o(),t("div")}const m=e(s,[["render",r]]);export{i as __pageData,m as default};
